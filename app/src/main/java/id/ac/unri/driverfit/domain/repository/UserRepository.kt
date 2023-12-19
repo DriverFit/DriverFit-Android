a09efb35f69246bb12b10f1b5f2d7feffd60f977
Ratu Yuri Meissy <ilikecat0101@gmail.com>
2023-12-19 16:04:03 +0700

    create user repo

---
+++ app/src/main/java/id/ac/unri/driverfit/domain/repository/UserRepository.kt
@@ -0,0 +1,27 @@
+package id.ac.unri.driverfit.domain.repository
+
+import id.ac.unri.driverfit.domain.model.User
+import kotlinx.coroutines.flow.Flow
+
+interface UserRepository {
+
+    suspend fun signUp(name: String, email: String, password: String): User
+
+    suspend fun signIn(email: String, password: String): User
+
+    suspend fun authGoogle(idToken: String): User
+
+    suspend fun signOut()
+
+    fun getUser(): Flow<User?>
+
+    suspend fun edit(token: String, name: String, password: String, photo: String?): User
+
+    suspend fun onboarding(isAlreadyOnboarding: Boolean)
+
+    fun onboarding(): Flow<Boolean>
+
+    suspend fun darkTheme(isDarkTheme: Boolean?)
+
+    fun darkTheme(): Flow<Boolean?>
+}
