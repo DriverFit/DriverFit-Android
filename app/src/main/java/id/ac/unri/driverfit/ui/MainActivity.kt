8bb9ad312155a29ff6c1ae4d9d5e8b8552e3813e
Ratu Yuri Meissy <ilikecat0101@gmail.com>
2023-12-21 16:50:39 +0700

    fix bottom navigation visibility and Support Navigate Up

--- app/src/main/java/id/ac/unri/driverfit/ui/MainActivity.kt
+++ app/src/main/java/id/ac/unri/driverfit/ui/MainActivity.kt
@@ -1,9 +1,9 @@
 package id.ac.unri.driverfit.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -24,7 +24,21 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+
+        navHostFragment.navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id in arrayOf(
+                    R.id.navigation_home,
+                    R.id.navigation_detection,
+                    R.id.navigation_profile
+                )
+            ) {
+                binding.navView.visibility = View.VISIBLE
+            } else {
+                binding.navView.visibility = View.GONE
+            }
+        }
 
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -33,7 +47,14 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.navView.setupWithNavController(navController)
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navHostFragment.navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        return navHostFragment.navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
